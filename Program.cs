40d735e28c390d81772ea1a4868a6ec5d7ae9ec5
Alex Halverson <alexhalverson@posteo.net>
2025-10-24 13:03:09 +1100

    feat: add swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 
